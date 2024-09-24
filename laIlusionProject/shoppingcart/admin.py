54cc23d6ce7224c77f748e3d139421d824d84220
cortegons <cortegons@eafit.edu.co>
2024-09-24 17:44:30 -0500

    Creado shoppingcart y agregada función de añadir, eliminar y ver carrito

--- laIlusionProject/shoppingcart/admin.py
+++ laIlusionProject/shoppingcart/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Carrito
 # Register your models here.
+
+admin.site.register(Carrito)
--- laIlusionProject/shoppingcart/urls.py
+++ laIlusionProject/shoppingcart/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     # Un ejemplo de vista básica
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('agregar/<int:producto_id>/', views.AgregarAlCarritoView.as_view(), name='agregar_al_carrito'),
+    path('carrito/', views.VerCarritoView.as_view(), name='ver_carrito'),
+    path('carrito/eliminar/<int:item_id>/', views.EliminarDelCarritoView.as_view(), name='eliminar_del_carrito'),
+
 ]
