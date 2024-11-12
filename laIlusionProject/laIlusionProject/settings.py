4a05394438d5350f416ce04df42523b39eb17312
MauricioDCH <mdcorreah@eafit.edu.co>
2024-11-12 13:46:40 -0500

    Add test to product app

--- laIlusionProject/laIlusionProject/settings.py
+++ laIlusionProject/laIlusionProject/settings.py
@@ -93,15 +93,14 @@ WSGI_APPLICATION = "laIlusionProject.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -112,6 +111,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
