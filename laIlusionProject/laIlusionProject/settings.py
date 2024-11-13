25696bb2d79e0d8cf442a6e80e2bc3f75bafe598
MauricioDCH <81777898+MauricioDCH@users.noreply.github.com>
2024-11-13 02:36:53 -0500

    Update settings.py

--- laIlusionProject/laIlusionProject/settings.py
+++ laIlusionProject/laIlusionProject/settings.py
@@ -100,14 +100,13 @@ WSGI_APPLICATION = "laIlusionProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 """
 DATABASES = {
     'default': {
@@ -119,7 +118,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
